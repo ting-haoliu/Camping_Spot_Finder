03084d85f7044dc5c51242914c133332671916a2
Ting Hao Liu <s0129qkak@outlook.com>
2025-02-11 23:07:00 -0800

    Added campgrounds Schema Connected mongoDB through mongoose

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('node:path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/YelpCamp', {
+    // ensure that you are using the latest MongoDB connection logic and avoid deprecation warnings
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Database connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
---
+++ models/campground.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //make this reference to let the reuse shortly
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
