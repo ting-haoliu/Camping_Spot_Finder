a398487d8c38bfe7b843a031edf7e9e409ed275b
Ting Hao Liu <s0129qkak@outlook.com>
2025-04-23 16:57:16 -0700

    modified the seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const mongoose = require('mongoose');
 const { cities } = require('./cities');
 const { descriptors, places } = require('./seedsHelpers');
